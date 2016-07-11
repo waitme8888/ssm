68b45aab7f9d5d23a5709e24d4d5b8796ad04d58
201605230171 <201605230171@201605230171-.creditease.corp>
2016-07-11 13:57:04 +0800

    shiro listener

--- src/main/java/com/waitme/ssm/controller/LoginController.java
+++ src/main/java/com/waitme/ssm/controller/LoginController.java
@@ -7,13 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 	
-	public static final String LOGIN_PAGE="login";
+	public static final String LOGIN_PAGE = "login";
+	public static final String INDEX_PAGE = "index";
 	
 	@RequestMapping("/login")
 	public String toLoginPage() {
 		
 		
 		return LOGIN_PAGE;
+	}
+	
+	@RequestMapping("/index")
+	public String toIndexpage() {
+		
+		return INDEX_PAGE;
 	}
 
 }
